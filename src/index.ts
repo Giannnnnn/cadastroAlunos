5b819299ea049a8af5848497ced377e6800a3dc5
Giovani Florek <giremio5@gmail.com>
2023-05-05 20:23:16 -0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import cors = require("cors")
 import { SqlDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { Agendamento } from "./entity/Agendamento"
 
 import * as express from "express"
 import { Request, Response } from "express"
@@ -14,84 +14,34 @@ SqlDataSource.initialize().then(async () => {
     app.use(cors(options))
     app.use(express.json())
 
-    const userRepo = SqlDataSource.getRepository(User)
+    const agendamentoReposity = SqlDataSource.getRepository(Agendamento)
 
     console.log(`Running on port 3000`)
 
-    app.get("/users", async function (req: Request, res: Response) {
-        const users = await userRepo.find()
+    app.get("/consultas", async function (req: Request, res: Response) {
+        const agendamentos = await agendamentoReposity.find()
         res.json({
-            users,
-            total: users.length
+            consultas: {
+                agendamentos
+            },
+            total: agendamentos.length
         })
     })
 
-    app.get("/users/alunos", async function (req: Request, res: Response) {
-        const users = await userRepo.find({
-            where: {
-                type: "aluno"
-            }
-        })
-        res.json({
-            users,
-            total: users.length
-        })
-    })
+    app.post("/consulta", async function (req: Request, res: Response) {
+        const agendamento = await agendamentoReposity.create(req.body)
 
-    app.get("/users/professores", async function (req: Request, res: Response) {
-        const users = await userRepo.find({
-            where: {
-                type: "professor"
-            }
-        })
-        res.json({
-            users,
-            total: users.length
-        })
-    })
-
-    app.post("/users/aluno", async function (req: Request, res: Response) {
-        console.log(req.body)
-
-        const user = await userRepo.create(req.body)
         try {
-            await userRepo.save(user).then((result) => {
+            await agendamentoReposity.save(agendamento).then((result) => {
                 if (result) {
                     return res.send(result)
                 } else {
-                    return res.status(404).send("Erro ao cadastrar aluno" + req.body.name)
+                    return res.status(404).send("Erro ao cadastrar agendamento de consulta" + req.body.name)
                 }
             })
         } catch (error) {
             return res.status(404).send(error)
         }
-    })
-
-    app.post("/users/professor", async function (req: Request, res: Response) {
-        console.log(req.body)
-
-        const user = await userRepo.create(req.body)
-        try {
-            await userRepo.save(user).then((result) => {
-                if (result) {
-                    return res.send(result)
-                } else {
-                    return res.status(404).send("Erro ao cadastrar professor" + req.body.name)
-                }
-            })
-        } catch (error) {
-            return res.status(404).send(error)
-        }
-    })
-
-    app.delete("/users/aluno/:id", async function (req: Request, res: Response) {
-        const results = await userRepo.delete(req.params.id)
-        return res.send(results)
-    })
-
-    app.delete("/users/aluno/:id", async function (req: Request, res: Response) {
-        const results = await userRepo.delete(req.params.id)
-        return res.send(results)
     })
 
     app.listen(3000)
